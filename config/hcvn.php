b888abbd7315fadc15c5e3e1b1da4c539a8452ec
the2004 <dongocthe232004@gmail.com>
2024-11-13 07:39:49 +0700

    .

--- config/hcvn.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'table_names' => [
-        'provinces' => 'provinces',
-        'districts' => 'districts',
-        'wards' => 'wards',
-    ]
-];
--- resources/views/user/send_mail.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.user')
-@section('content')
-<h3>{{$name}}</h3>
-<p>{{$message}}</p>
-@endsection
